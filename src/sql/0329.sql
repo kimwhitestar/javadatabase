c00d004ce59b6c3a6a3c2610c9afae337631823c
kimwhitestar <peculiar.25@hanmail.net>
2022-03-29 18:32:05 +0900

    0329_18:31 0329.sql신규추가

---
+++ src/sql/0329.sql
@@ -0,0 +1,19 @@
+show tables;
+
+desc aaa;
+
+drop table aaa;
+
+create table aaa (
+	name varchar(20) not null,
+	age int default 20,
+	gender char(2) default 'm',
+	joinday datetime
+);
+
+insert into aaa values 
+('홍길동',25,default,default);
+insert into aaa values 
+('홍길순',29,'f',default);
+insert into aaa values 
+('김말숙',35,'f','2020-1-5');
